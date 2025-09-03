314cb691f1709d15ea5bbf87dae8646d321c9047
João Augusto <jaugustovpires@gmail.com>
2025-09-02 21:34:34 -0300

    feat: home page content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>olá</h1>
+    <section class="home_pg">
+        <h1 class="title">Que tal organizar sua rotina semanal <br>com nosso serviço de <span> anotações?</span></h1>
+        <p class="subtitle">Com nossa ferramenta, você vai poder criar, organizar e gerenciar <br>todas as tarefas do dua de forma totalmente gratuita</p>
+        @include('components.button', ['text' => 'COMEÇAR HOJE MESMO', 'class' => 'btn_login'])
+    </section>
 @endsection
